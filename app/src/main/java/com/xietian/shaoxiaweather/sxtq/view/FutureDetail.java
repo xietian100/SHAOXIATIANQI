5b2d55501dec38c2ebaaf250d26a53f777799f51
xietian100 <2559472677@qq.com>
2017-02-03 17:30:42 +0800

    hellocharts显示

--- app/src/main/java/com/xietian/shaoxiaweather/sxtq/view/FutureDetail.java
+++ app/src/main/java/com/xietian/shaoxiaweather/sxtq/view/FutureDetail.java
@@ -13,10 +13,16 @@ import com.xietian.shaoxiaweather.sxtq.utils.UIUtils;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
+import lecho.lib.hellocharts.gesture.ContainerScrollType;
+import lecho.lib.hellocharts.gesture.ZoomType;
+import lecho.lib.hellocharts.model.Axis;
+import lecho.lib.hellocharts.model.AxisValue;
 import lecho.lib.hellocharts.model.Line;
 import lecho.lib.hellocharts.model.LineChartData;
 import lecho.lib.hellocharts.model.PointValue;
+import lecho.lib.hellocharts.model.ValueShape;
 import lecho.lib.hellocharts.view.LineChartView;
 
 /**
@@ -50,21 +56,26 @@ public class FutureDetail extends LinearLayout{
         horizontalListViewAdapter=new HorizontalListViewAdapter(UIUtils.getContext());
         horizontalListViewAdapter.notifyDataSetChanged();
         horizontalListView.setAdapter(horizontalListViewAdapter);
-
         lineChart= (LineChartView) view.findViewById(R.id.line_chart);
+
+
         List<PointValue> values = new ArrayList<PointValue>();
         values.add(new PointValue(0, 2));
         values.add(new PointValue(1, 4));
         values.add(new PointValue(2, 3));
-        values.add(new PointValue(3, 4));
-//In most cased you can call data model methods in builder-pattern-like manner.
+
+
+        //In most cased you can call data model methods in builder-pattern-like manner.
         Line line = new Line(values).setColor(Color.BLUE).setCubic(true);
         List<Line> lines = new ArrayList<Line>();
         lines.add(line);
+
         LineChartData data = new LineChartData();
+        data.setValueLabelBackgroundEnabled(false);
         data.setLines(lines);
-        lineChart.setLineChartData(data);
-        lineChart.setVisibility(View.VISIBLE);
+
+        LineChartView chart = new LineChartView(UIUtils.getContext());
+        chart.setLineChartData(data);
 
     }
 
